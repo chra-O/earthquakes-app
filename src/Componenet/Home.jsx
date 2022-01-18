6a94679a821b34dc3e4004446e5aaf49cd7a0acb
Chra <chra.osman99@gmail.com>
2022-01-18 15:24:58 +0300

    adding fetch

--- src/Componenet/Home.jsx
+++ src/Componenet/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    return (
-        <div>
-            home
-        </div>
+  const [location, setLocation] = useState({});
+
+  let hadelCilick = () => {
+    fetch(
+      "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-01-01&limit=20"
     )
+      .then((response) => response.json())
+
+      .then((response) => setLocation(response));
+  };
+
+  // ********************************
+  useEffect(() => {
+    hadelCilick();
+  }, []);
+
+  //////////
+  return <div>{location.type}</div>;
 }
