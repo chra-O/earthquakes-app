a2c6950f5aee3c995ad23b0be2b5df6177865a0a
Chra <chra.osman99@gmail.com>
2022-01-18 14:07:26 +0300

    adding route to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Detail from "./Componenet/Detail";
+import Home from "./Componenet/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details/:eventid" element={<Detail />} />
+        </Routes>
       </header>
     </div>
   );
