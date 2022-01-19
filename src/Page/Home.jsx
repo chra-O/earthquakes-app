63d08c6d7d5ac17762213eb4612fd611e51f9a6d
Chra <chra.osman99@gmail.com>
2022-01-19 19:47:00 +0300

    adding new commite

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -4,35 +4,51 @@ import { Card, Button } from "react-bootstrap";
 
 export default function Home() {
   const [location, setLocation] = useState({});
-
-  let hadelCilick = () => {
-    fetch(
-      "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-01-01&limit=20"
-    )
-      .then((response) => response.json())
-
-      .then((response) => setLocation(response));
-  };
-
+  const [featuer, setfeauer] = useState();
   // ********************************
-  useEffect(() => {
-    hadelCilick();
-  });
 
   //////////
+  useEffect(() => {
+    fetch(
+      "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-01-01&endtime=2022-12-01&limit=10"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setLocation(data);
+
+        data.map((index) => {
+          console.log(index.metadata);
+        });
+      });
+  }, []);
 
   return (
     <>
-      <Card className=" cardstyle">
+      <p>{location["bbox"]}</p>
+
+      {/* {location.map((product) => {
+        return (
+          <div>
+            {product.type}
+         </div>
+          
+        );
+      })} */}
+      {/* <Card className=" cardstyle">
         <Card.Img style={{ width: "18rem" }} />
         <Card.Body>
+         
           <Card.Title>{location.type}</Card.Title>
           <hr></hr>
-          <Card.Text  id="textstyling">place : {location.type}</Card.Text>
+          <Card.Text id="textstyling">place :</Card.Text>
 
-          <Button id="button" variant="success">Go somewhere</Button>
+          <Button id="button" variant="success">
+            Go somewhere
+          </Button>
         </Card.Body>
-      </Card>
+      </Card> */}
     </>
   );
 }
