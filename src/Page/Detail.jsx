48aadc3ec3e201b96031748080cb05c21dfcf326
Chra <chra.osman99@gmail.com>
2022-01-21 14:58:39 +0300

    adding latitude and longitude

--- src/Page/Detail.jsx
+++ src/Page/Detail.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import { Card, Button } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { Card } from "react-bootstrap";
 export default function Detail() {
   const { id } = useParams();
-  const [s, ss] = useState([{}]);
+  const [s, ss] = useState([]);
 
   ////////////////////////
   useEffect(() => {
@@ -18,7 +18,27 @@ export default function Detail() {
         ss(data.properties);
       });
   }, []);
-  //i have problem with map . so i can access longitude and latitude
+
+  let latitude;
+  if (s) {
+    if (s.products) {
+      if (s.products.origin[0]) {
+        if (s.products.origin[0].properties) {
+          latitude = s.products.origin[0].properties.latitude;
+        }
+      }
+    }
+  }
+  let longitude;
+  if (s) {
+    if (s.products) {
+      if (s.products.origin[0]) {
+        if (s.products.origin[0].properties) {
+          longitude = s.products.origin[0].properties.longitude;
+        }
+      }
+    }
+  }
 
   return (
     <>
@@ -28,7 +48,8 @@ export default function Detail() {
           <hr></hr>
           <Card.Text id="textstyling">Time : {s.time}</Card.Text>
           <Card.Text id="textstyling">Type : {s.type}</Card.Text>
-          <Card.Text id="textstyling"></Card.Text>
+          <Card.Text id="textstyling"> latitude : {latitude} </Card.Text>
+          <Card.Text id="textstyling"> longitude : {longitude} </Card.Text>
         </Card.Body>
       </Card>
     </>
