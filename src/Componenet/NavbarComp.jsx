5b9ddac69939e3524be281c3aca47882f0aa5c1e
Chra <chra.osman99@gmail.com>
2022-01-21 14:26:30 +0300

    changeing href to link

--- src/Componenet/NavbarComp.jsx
+++ src/Componenet/NavbarComp.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
 export default function NavbarComp() {
   return (
     <div>
       <Navbar bg="light" variant="light ">
         <Container>
-          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Navbar.Brand>
+            <Link id="navbarStyle" to="/">Home</Link>
+          </Navbar.Brand>
         </Container>
       </Navbar>
     </div>
